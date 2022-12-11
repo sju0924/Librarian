44da3ce7111b60e6bb93939d6439a76b497540bf
jeon sohn <fe91990@gmail.com>
2022-12-12 02:21:04 +0900

    Add structure to serve data from db to requesting client

--- QuerOp/src/test/java/com/librarian/spring/dto/BookDto.java
+++ QuerOp/src/test/java/com/librarian/spring/dto/BookDto.java
@@ -1,4 +1,35 @@
 package com.librarian.spring.dto;
 
+import com.librarian.spring.model.testClass;
+import lombok.*;
+
+import javax.persistence.Column;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BookDto {
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class testInfo {
+        private int ISBN;
+        private String title;
+        private String author;
+        private Integer year;
+
+        public static testInfo of(testClass test) {
+            return testInfo.builder()
+                    .ISBN(test.getISBN())
+                    .title(test.getTitle())
+                    .author(test.getAuthor())
+                    .year(test.getYear())
+                    .build();
+        }
+
+        public static List<testInfo> of(List<testClass> tests) {
+            return tests.stream().map(testInfo::of).collect(Collectors.toList());
+        }
+    }
 }
